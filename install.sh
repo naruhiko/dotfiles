a0907359e3b1c1f5b75c3cf1fb196041d7f4de5a
Naruhiko Nagata <nullhiko@astranox.com>
2020-12-23 11:42:23 +0900

    change func installing zsh

--- install.sh
+++ install.sh
@@ -3,14 +3,13 @@
 set -ue
 cd
 
-
 echo -e "\n
-             .__  .__    ____   ____.___   _____    \n
-  ____  __ __|  | |  |   \   \ /   /|   | /     \   \n
- /    \|  |  \  | |  |    \   Y   / |   |/  \ /  \  \n
-|   |  \  |  /  |_|  |__   \     /  |   /    Y    \ \n
-|___|  /____/|____/____/    \___/   |___\____|__  / \n
-     \/                                         \/  \n "
+             .__  .__    ____   ____.___   _____    
+  ____  __ __|  | |  |   \   \ /   /|   | /     \   
+ /    \|  |  \  | |  |    \   Y   / |   |/  \ /  \  
+|   |  \  |  /  |_|  |__   \     /  |   /    Y    \ 
+|___|  /____/|____/____/    \___/   |___\____|__  / 
+     \/                                         \/  "
 
 echo " ------------ Homebrew ------------"
 read -p "Install Homebrew ? (y/n)" Answer < /dev/tty
@@ -22,45 +21,41 @@ case ${Answer} in
     else
       echo "Start Install Homebrew..."
       apt install build-essential curl file
-
       git clone https://github.com/Homebrew/brew ~/linuxbrew/.linuxbrew/Homebrew
       mkdir ~/linuxbrew/.linuxbrew/bin
       ln -sf ~/linuxbrew/.linuxbrew/Homebrew/bin/brew ~/linuxbrew/.linuxbrew/bin
       eval $(~/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-
       echo "Homebrew Installed" 
     fi ;;
   n|N)
     echo "Skipped" ;;
-
 esac
 
 echo "------------ zsh ------------"
 read -p "Change the Shell into zsh ? (y/n)" Answer < /dev/tty
 case ${Answer} in
   y|Y)
-    echo 'Defaults env_keep += "PATH"' >> /etc/sudoers
-    sed -i -e "s/Defaults secure_path/\#Defaults secure_path/g" /etc/sudoers
-    brew install zsh zsh-syntax-highlighting
-    echo '/usr/local/bin/zsh' >> /etc/shells 
-    chsh -s /usr/local/bin/zsh 
+    if [[ -d ~/linuxbrew/.linuxbrew/bin/zsh ]]
+    then
+      echo "Already exist"
+    else
+      echo 'Defaults env_keep += "PATH"' >> /etc/sudoers
+      sed -i -e "s/Defaults secure_path/\#Defaults secure_path/g" /etc/sudoers
+      brew install zsh zsh-syntax-highlighting
+      echo '/root/linuxbrew/.linuxbrew/bin/zsh' >> /etc/shells 
+    fi
+    chsh -s /root/linuxbrew/.linuxbrew/bin/zsh
     FILE="${HOME}/.bash_profile"
-
         if [[ -e ${FILE} ]]; then
           source ${FILE} >> ~/.zshrc
         elif [[ ! -e ${FILE} ]]; then
           touch ${FILE}
         fi
-
     source ~/.zshrc
-
     ;;
-
   n|N)
     echo "Skipped" ;;
 esac
-
 
 echo "---------- nvim & tmux ----------"
 echo "processing..."
@@ -73,9 +68,7 @@ echo "finished"
 echo "---------- zprezto ----------"
 echo "processing..."
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
 setopt EXTENDED_GLOB for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" done
-
 echo "finished"
 
 echo "---------- cloning naruhiko mods. ----------"
